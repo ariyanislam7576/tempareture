61de1c752a3b7b9d60684c3903fbbea046754d47
unknown <ma5665711@gmail.com>
2021-09-04 04:06:46 -0700

    hot

--- temperature-hot-master/tempareture.js
+++ temperature-hot-master/tempareture.js
@@ -19,14 +19,24 @@ const loadTemp = () => {
     
 }
 const displayTemp = data => {
+    if(data.length === 0){
+        alert('please input something')
+    }
+    else if(
+        data.massage === "city not found"
+    ){
+        alert('something went wrong')
+    }
     const tempareture = Math.floor(data.list[0].main.temp / 10)
     const containData = document.getElementById('contain-city')
     containData.textContent = '';
+    const url = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`
     const div = document.createElement('div')
     div.innerHTML = `
+        <img src="${url}" alt="">
             <h1>${data.city.name} ${data.city.country}</h1>
             <h3><span>${tempareture} </span>&deg;C</h3>
-             <h1 class="lead"> Cloud: ${data.list[0].clouds.all}</h1>
+            <h1 class="lead">${data.list[0].weather[0].main}</h1>
     `
     containData.appendChild(div)
 }
